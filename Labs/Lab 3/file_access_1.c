5c0b6a8ee82dc9ac771d856302d301578264a24d
Johan Fernandes <jfern090@uottawa.ca>
2019-10-01 16:42:38 -0400

    Updated of Lab3

--- Labs/Lab 3/file_access_1.c
+++ Labs/Lab 3/file_access_1.c
@@ -12,35 +12,46 @@ int main(void) {
   //Simple file read
   fptr = fopen("string_data.txt","r");
   
+  //Print the file contents here:
   printf("File contents are: \n");
   while(fgets(sentence, sizeof(sentence), fptr))
     printf("%s", sentence);
  
-  printf("\nEnd of file access \n");
-
+  printf("\n\nEnd of file access \n\n");
+  //close the file access
   fclose(fptr);
 
 
-
   //Clearing the file and adding new content
+  //w+ will clear the content and provide read-write access or it will create a new file
+  //r+ will provide read-write access or it will throw an error if the file does not exist
   fptr = fopen("string_data.txt","w+");
 
-  printf("File contents are: \n");
+  printf("\nFile contents are: \n\n");
   while(fgets(sentence, sizeof(sentence), fptr))
     printf("%s", sentence);  
 
-  printf("There's nothing in the file \n");
+  printf("Oop's it looks like all the content has been cleared, no thanks to w+\n\n");
+
   printf("Enter some new content \n");
+  //This is one way to get a string input.
+  //Scanf will also discards the new line character
   scanf("%[^\n]s", sentence);
 
+  //Print the string you just got onto the blank file
   fprintf(fptr, "%s\n", sentence);
 
-  printf("Enter another line \n");
-  c = getchar();     //For the new line char
-  gets(sentence);
   
-  fprintf(fptr, "%s\n", sentence);
+  printf("\nCool, now enter another line\n");
+  c = getchar();     //Remove the new line char
+  //Better  way to get a string input.
+  fgets(sentence, sizeof(sentence), stdin);
 
+  // c = getchar();     //Remove the new line char
+  //Print the new input onto the next line on the file
+  //Notice no \n here becuase fgets will inlcude that in string as well.
+  fprintf(fptr, "%s", sentence);
+  //close the file access
   fclose(fptr);
 
 
@@ -48,29 +59,31 @@ int main(void) {
   //Appending content to the file
   fptr = fopen("string_data.txt","a+");
 
-  printf("Enter some new content \n");
-  gets(sentence);  
-
+  printf("\n\nEnter some new content to append\n");
+  //By now you know this is better
+  fgets(sentence, sizeof(sentence), stdin); 
+  //Notice no \n here becuase fgets will inlcude that in string as well.
   fprintf(fptr, "%s", sentence);
-
+  //close the file access
   fclose(fptr);
 
   printf("\n");
 
-  //Appending in a different manner
+  //Appending data somwhere in the middle
   fptr = fopen("string_data.txt","r+");
 
-  printf("File contents are: \n");
+  printf("\nFile contents are: \n");
   while(fgets(sentence, sizeof(sentence), fptr))
     printf("%s", sentence); 
   
-  // fseek(fptr, -4, SEEK_END);
+  //fseek will move th pointer to a specific location in the file
+  //in this case it will start at the last word in the file and begin printing from the 3rd place to the left of the last character.
   fseek(fptr, -4, SEEK_END);
   
   printf("Enter some new content \n");
-  gets(sentence);  
+  fgets(sentence, sizeof(sentence), stdin);
 
   fprintf(fptr, "%s", sentence);
-
+  //close the file access
   fclose(fptr);
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 3130-DBMS
 Lab and tutorial files for students
+
+
+## update 1 : 01/10/2019
+Labs/Lab3/file_access_1.c is now fixed. Two methods to read a string have been displayed
